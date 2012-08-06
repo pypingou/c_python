2c07de7042490264c22b698b5eae888954f2e675
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-08-06 13:55:06 +0200

    Add C++ example

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup, Extension
  
 module1 = Extension('fib', sources = ['fibmodule.c'])
+module2 = Extension("hello", ["hellomodule.cpp"], libraries = ["boost_python"])
  
 setup (name = 'c_python',
         version = '1.0',
         description = 'This is a demo package extending python with C',
-        ext_modules = [module1]
+        ext_modules = [module1, module2]
         )
