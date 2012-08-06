60c868c341ec48e7ab86e619800a8dbdfdcb17dc
Pierre-Yves Chibon <pingou@pingoured.fr>
2012-08-06 14:50:00 +0200

    Implement an example function with a list as argument

--- hellomodule.cpp
+++ hellomodule.cpp
@@ -1,18 +1,32 @@
 #include <string>
- 
 using namespace std;
- 
-std::string say_hello(const string name) {
+
+#include <boost/python/module.hpp>
+#include <boost/python/def.hpp>
+#include <boost/python/list.hpp>
+#include <boost/python/extract.hpp>
+#include <boost/python/ssize_t.hpp>
+using namespace boost::python;
+
+string say_hello(const string name)
+{
     string out = "Hello ";
     out = out + name + "!\n";
     return out;
 }
- 
-#include <boost/python/module.hpp>
-#include <boost/python/def.hpp>
-using namespace boost::python;
- 
+
+int sum(list input)
+{
+    int out = 0;
+    ssize_t n = len(input);
+    for(ssize_t i=0; i<n; i++) {
+        out = out + extract<double>(input[i]);
+    }
+    return out;
+}
+
 BOOST_PYTHON_MODULE(hello)
 {
     def("say_hello", say_hello);
+    def("sum", sum);
 }
